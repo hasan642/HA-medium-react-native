5151075227cd25cd8bec987b48f7a6323a1e9894
hasan_1995 <hasan.khaled.alawneh@gmail.com>
2020-11-15 03:37:24 +0200

    fix header

--- src/navigation/helpers.ts
+++ src/navigation/helpers.ts
@@ -1,9 +1,59 @@
 /**
  * A function helpers for navigation.
  */
-import { Navigation } from "react-native-navigation";
-import { screens } from './screens';
-import { color } from "theme";
+import {
+    Navigation,
+    ImageSystemSource
+} from "react-native-navigation";
+import {
+    screens,
+    Screens
+} from './screens';
+import {
+    color,
+    responsiveFontSize
+} from 'theme';
+import { translate, isRTL } from "i18n";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+/**
+ * interfaces and types.
+ */
+interface NavigationIcons {
+    MENU_ICON: ImageSystemSource;
+    BACK_ICON: ImageSystemSource;
+};
+
+/**
+ * A function helper that loads and prepares the
+ * * required icons image sources.
+ */
+async function getNavIcons(): Promise<NavigationIcons> {
+
+    /**
+     * load icons.
+     */
+    const icons = await Promise.all([
+        Ionicons.getImageSource(
+            'md-menu',
+            responsiveFontSize(24),
+        ),
+
+        Ionicons.getImageSource(
+            isRTL() ? 'ios-chevron-forward' : 'ios-chevron-back',
+            responsiveFontSize(4),
+        ),
+    ]);
+
+    /**
+     * return icons.
+     */
+    return {
+        MENU_ICON: icons[0],
+        BACK_ICON: icons[1],
+    };
+
+};
 
 /**
  * A function helper that goes to app.
@@ -12,7 +62,7 @@ export function goToApp() {
     Navigation.setRoot({
         root: {
             sideMenu: {
-                id: "sideMenu",
+                id: "SIDE_MENU",
                 left: {
                     component: {
                         id: screens.SIDE_MENU,
@@ -21,7 +71,7 @@ export function goToApp() {
                 },
                 center: {
                     stack: {
-                        id: "App",
+                        id: "CENTER_STACK",
                         children: [{
                             component: {
                                 id: screens.HOME_SCREEN,
@@ -55,9 +105,31 @@ export function goToAuth() {
 };
 
 /**
+ * A function helper to navigate.
+ */
+export function goTo(
+    componentId: string,
+    pushedScreen: Screens,
+    props?: { [key: string]: any }
+) {
+    Navigation.push(
+        componentId,
+        {
+            component: {
+                id: screens[pushedScreen],
+                name: screens[pushedScreen],
+                passProps: props,
+            }
+        },
+    );
+};
+
+/**
  * A function helper that set default options for navigation.
  */
-export function setDefaultOptions() {
+export async function setDefaultOptions() {
+    const icons = await getNavIcons();
+
     Navigation.setDefaultOptions({
         statusBar: {
             style: 'dark',
@@ -65,23 +137,23 @@ export function setDefaultOptions() {
             translucent: true
         },
         layout: {
-
-            /**
-             * will be handled after "i18n" modal
-             * * added.
-             */
-            direction: 'rtl',
-
+            direction: 'ltr',
             backgroundColor: color.light,
-            orientation: ['portrait']
+            orientation: ['portrait'],
         },
         topBar: {
             animate: true,
-            hideOnScroll: true,
-            drawBehind: false
+            drawBehind: false,
+            noBorder: true,
+            borderHeight: 0,
+            backButton: {
+                title: translate('common.back'),
+                showTitle: false,
+                icon: icons.BACK_ICON
+            },
         },
         sideMenu: {
-            openGestureMode: 'entireScreen'
+            openGestureMode: 'entireScreen',
         },
         popGesture: true
     })
